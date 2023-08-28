5b786b0874492020b61acfcfb6663328786defdc
KJS <digyrh456789@gmail.com>
2023-08-28 17:52:00 +0900

    feat: Add modal window

--- todolist/pages/index.tsx
+++ todolist/pages/index.tsx
@@ -2,20 +2,43 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import List from './list'
 
+
 const Home: NextPage = () => {
+  const [Opening, setOpening] = useState(false);
+  const openModal = () => {
+    setOpening(!Opening);
+  }
+  const changejudge = () => {
+    setOpening(!Opening);
+  }
+  const addtodolist = () => {
+    setOpening(!Opening);
+  }
   return (
-    <div className={styles.container}>
-      <h1 className={styles.logo}>DO!</h1>
-      <main className={styles.main}>
-        <h1 className={styles.maintext}>
-          오늘의 <span className={styles.maining}>할 일</span>을 등록해주세요
-        </h1>
-      </main>
-      <List />
-    </div>
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>DO!</h1>
+        <main className={styles.main}>
+          <h1 className={styles.maintext}>
+            오늘의 <span className={styles.maining}>할 일</span>을 등록해주세요
+          </h1>
+        </main>
+        {Opening ? <div className={styles.modalbackdrop}>
+          <div className={styles.realmodal}>
+            <h1 className={styles.backbutton} onClick={changejudge}>X</h1>
+            <input placeholder='Write what are you going to do' className={styles.addto}></input>
+            <button className={styles.addbutton} onClick={addtodolist}>추가하기</button>
+          </div>
+        </div> : null}
+        <div className={styles.userlist}>
+          <List />
+          <img src="/images/plus.png" className={styles.plusbutton} onClick={openModal}></img>
+        </div>
+      </div>
+    </>
   )
 }
 
--- todolist/pages/list.tsx
+++ todolist/pages/list.tsx
@@ -2,21 +2,23 @@ import { NextPage } from "next";
 import styles from '../styles/List.module.css';
 import jsondata from './data/listdata.json';
 import { json } from "stream/consumers";
+import { useEffect, useState, useContext } from 'react';
+
 const List = () => {
+    let userslist: Array<string> = [];
+
     return (
         <>
-            <div className={styles.userlist}>
-                {jsondata.map(todolist => (
-                    <div key={todolist.id} className={styles.listbox}>
-                        <p className={styles.listname}>{todolist.description}</p>
-                        <div className={styles.imgbutton}>
-                            <img src="/images/yes.png" className={styles.completebutton}></img>
-                            <img src="/images/remove.png" className={styles.deletebutton}></img>
-                        </div>
+            {jsondata.map(todolist => (
+                <div key={todolist.id} className={styles.listbox}>
+                    <p className={styles.listname}>{todolist.description}</p>
+                    <div className={styles.imgbutton}>
+                        <img src="/images/yes.png" className={styles.completebutton}></img>
+                        <img src="/images/remove.png" className={styles.deletebutton}></img>
                     </div>
-                ))}
-                <img src="/images/plus.png" className={styles.plusbutton}></img>
-            </div>
+                </div>
+            ))}
+
         </>
     )
 }
