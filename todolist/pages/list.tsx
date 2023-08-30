a3eaa45fe5a2143ef84826b8695ec9e711767511
KJS <digyrh456789@gmail.com>
2023-08-31 08:28:57 +0900

    feat: get json data from json server

--- todolist/pages/list.tsx
+++ todolist/pages/list.tsx
@@ -1,24 +1,39 @@
 import { NextPage } from "next";
 import styles from '../styles/List.module.css';
-import jsondata from './data/listdata.json';
 import { json } from "stream/consumers";
 import { useEffect, useState, useContext } from 'react';
 
 const List = () => {
-    let userslist: Array<string> = [];
-
+    const [userlist, setUserlist] = useState([]);
+    function getposting() {
+        const xhr = new XMLHttpRequest(); // XMLHttpRequest 객체 생성
+        xhr.open("GET", "http://localhost:5000/lists");
+        xhr.setRequestHeader("content-type", "application/json");
+        xhr.send();
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                const res = JSON.parse(xhr.response);
+                const descriptions = res.map((item: any) => item.description); // 각 인덱스의 description 값만 추출
+                setUserlist(descriptions);
+            } else {
+                console.log(xhr.status, xhr.statusText);
+            }
+        }
+    }
+    useEffect(() => {
+        getposting();
+    }, []);
     return (
         <>
-            {jsondata.map(todolist => (
-                <div key={todolist.id} className={styles.listbox}>
-                    <p className={styles.listname}>{todolist.description}</p>
+            {userlist.map(todolist => (
+                <div key={todolist} className={styles.listbox}>
+                    <p className={styles.listname}>{todolist}</p>
                     <div className={styles.imgbutton}>
-                        <img src="/images/yes.png" className={styles.completebutton}></img>
+                        <img src="/images/yes.png" className={styles.completebutton} onClick={getposting}></img>
                         <img src="/images/remove.png" className={styles.deletebutton}></img>
                     </div>
                 </div>
             ))}
-
         </>
     )
 }
