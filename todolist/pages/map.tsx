0896872892546513a08c0b2e9966e75d378cc023
ShimJiHoon <wlgns2223@gmail.com>
2023-10-10 10:51:35 +0900

    chore: .

--- todolist/pages/map.tsx
+++ todolist/pages/map.tsx
@@ -3,52 +3,82 @@ import React from "react";
   mdt: { max: '1280px'},
   wmob: { max: '960px' },
   smob: { max: '480px'},
+  http://banobagiwellness.com
 */
 export const Map: React.FC = () => {
     return (
         <section className="mx-auto max-w-screen-xl">
-  <div className="flex md:px-8 md:w-full max-w-full md:space-between md:h-screen md:bg-gradient-to-t md:from-lime-50 md:to-white bg-none md:flex-row flex-col-reverse h-auto p-0">
-    <div className="md:w-6/12 md:h-full md:pt-32 md:pb-36 md:px-12 text-center items-center justify-center md:pb-24 md:px-8 h-96 w-full p-0 mb-12">
-      <div className="overflow-hidden w-full h-full">
-        <img src="/images/JANDA.png" alt="JANDA" className="mx-auto w-full transform transition-transform ease-in-out" />
-      </div>
+            <div className="flex md:px-8 md:w-full max-w-full md:space-between md:h-screen md:bg-gradient-to-t md:from-lime-50 md:to-white bg-none md:flex-row flex-col-reverse h-auto p-0">
+                <div className="md:w-6/12 md:h-full md:pt-32 text-center items-center justify-center md:pb-24 md:px-8 h-96 w-full p-0 mb-12">
+                    <div className="overflow-hidden w-full h-full">
+                        <img
+                            src="/images/JANDA.png"
+                            alt="JANDA"
+                            className="mx-auto w-full transform transition-transform ease-in-out"
+                        />
+                    </div>
                 </div>
                 <div className="md:w-6/12 h-full pt-32 pb-36 px-12 md:block hidden">
                     <div className="border-solid border-black border-t-2 ">
-                            <p className="lg:text-3xl pt-12 px-2 text-2xl">진료시간 및 오시는 길 안내</p>
-                            <p className="mt-12 mb-4 px-2 lg:text-2xl text-gray-400 text-xl">오시는 길</p>
-                            <div className="flex justify-content px-2 items-end lg:flex-norwap flex-wrap">
-                                <div className="lg:w-7/12 lg:text-2xl w-full text-xl">
-                                    <p>
-                                        서울특별시 강남구 논현로 523 4층<br/>
-                                        (역삼동, 노바빌딩)
-                                    </p>
-                                    <p className="mt-2">
-                                        02-538-6508
-                                    </p>
-                                </div>
-                                <div className="xl:w-5/12 text-right w-full">
-                                    <p className="flex justify-end items-center"><span className="w-5 h-5 bg-green-500 text-white text-center rounded-full mr-1.5 leading-5 text-xs ">2</span>호선 역삼역 6번 출구 앞</p>
-                                    <p className="flex justify-end items-center mt-1"><span className="w-5 h-5 bg-yellow-600 text-white text-center rounded-full mr-1.5 leading-5 text-xs">9</span>호선 언주역 7번 출구 앞</p>
-                                </div>
-                            </div>
-                            <div className="mt-12 mb-4 pt-12 px-2 border-solid border-black border-t">
-                                <p className="xl:text-2xl text-gray-400 text-xl">진료시간 안내</p>
-                            </div>
-                            <div className="px-2">
-                                <p className="flex items-center justify-start">
-                                    <span className="pr-2.5 xl:text-2xl font-normal tracking-wider text-xl">월 ~ 금</span>
-                                    <span className="font-medium tracking-wide xl:text-2xl text-xl ">09:00 ~ 18:00</span>
+                        <p className="lg:text-3xl pt-12 px-2 text-2xl">
+                            진료시간 및 오시는 길 안내
+                        </p>
+                        <p className="mt-12 mb-4 px-2 lg:text-2xl text-gray-400 text-xl">
+                            오시는 길
+                        </p>
+                        <div className="flex justify-content px-2 items-end lg:flex-norwap flex-wrap">
+                            <div className="lg:w-7/12 lg:text-2xl w-full text-xl">
+                                <p>
+                                    서울특별시 강남구 논현로 523 4층
+                                    <br />
+                                    (역삼동, 노바빌딩)
                                 </p>
-                                <p className="flex items-center justify-start mt-2">
-                                    <span className="pr-2.5 xl:text-2xl font-normal tracking-wider text-xl">월 ~ 금</span>
-                                    <span className="font-medium tracking-wide xl:text-2xl text-xl">09:00 ~ 18:00</span>
+                                <p className="mt-2">02-538-6508</p>
+                            </div>
+                            <div className="xl:w-5/12 text-right w-full">
+                                <p className="flex justify-end items-center">
+                                    <span className="w-5 h-5 bg-green-500 text-white text-center rounded-full mr-1.5 leading-5 text-xs ">
+                                        2
+                                    </span>
+                                    호선 역삼역 6번 출구 앞
                                 </p>
-                                <p className="flex items-center justify-start mt-2">
-                                    <span className="pr-2.5 xl:text-2xl font-normal tracking-wider text-xl">월 ~ 금</span>
-                                    <span className="font-medium tracking-wide xl:text-2xl text-xl ">09:00 ~ 18:00</span>
+                                <p className="flex justify-end items-center mt-1">
+                                    <span className="w-5 h-5 bg-yellow-600 text-white text-center rounded-full mr-1.5 leading-5 text-xs">
+                                        9
+                                    </span>
+                                    호선 언주역 7번 출구 앞
                                 </p>
                             </div>
+                        </div>
+                        <div className="mt-12 mb-4 pt-12 px-2 border-solid border-black border-t">
+                            <p className="xl:text-2xl text-gray-400 text-xl">진료시간 안내</p>
+                        </div>
+                        <div className="px-2">
+                            <p className="flex items-center justify-start">
+                                <span className="pr-2.5 xl:text-2xl font-normal tracking-wider text-xl">
+                                    월 ~ 금
+                                </span>
+                                <span className="font-medium tracking-wide xl:text-2xl text-xl ">
+                                    09:00 ~ 18:00
+                                </span>
+                            </p>
+                            <p className="flex items-center justify-start mt-2">
+                                <span className="pr-2.5 xl:text-2xl font-normal tracking-wider text-xl">
+                                    월 ~ 금
+                                </span>
+                                <span className="font-medium tracking-wide xl:text-2xl text-xl">
+                                    09:00 ~ 18:00
+                                </span>
+                            </p>
+                            <p className="flex items-center justify-start mt-2">
+                                <span className="pr-2.5 xl:text-2xl font-normal tracking-wider text-xl">
+                                    월 ~ 금
+                                </span>
+                                <span className="font-medium tracking-wide xl:text-2xl text-xl ">
+                                    09:00 ~ 18:00
+                                </span>
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className="md:hidden block w-full py-24 px-16">
@@ -61,15 +91,21 @@ export const Map: React.FC = () => {
                         </div>
                         <div className="w-full max-w-lg px-2 text-xl">
                             <p className="flex justify-start items-center">
-                                <span className="w-28 pr-2.5 font-normal tracking-wider ">월 ~ 금</span>
+                                <span className="w-28 pr-2.5 font-normal tracking-wider ">
+                                    월 ~ 금
+                                </span>
                                 <span className="font-mideum tracking-wide">09:00 ~ 18:00</span>
                             </p>
                             <p className="flex justify-start items-center mt-2">
-                                <span className="w-28 pr-2.5 font-normal tracking-wider">월 ~ 금</span>
+                                <span className="w-28 pr-2.5 font-normal tracking-wider">
+                                    월 ~ 금
+                                </span>
                                 <span className="font-mideum tracking-wide">09:00 ~ 18:00</span>
                             </p>
                             <p className="flex justify-start items-center mt-2">
-                                <span className="w-28 pr-2.5 font-normal tracking-wider">월 ~ 금</span>
+                                <span className="w-28 pr-2.5 font-normal tracking-wider">
+                                    월 ~ 금
+                                </span>
                                 <span className="font-mideum tracking-wide">09:00 ~ 18:00</span>
                             </p>
                         </div>
@@ -78,15 +114,23 @@ export const Map: React.FC = () => {
                         </div>
                         <div className="flex items-end justify-between w-full max-w-lg flex-wrap px-2">
                             <div className="w-full text-xl">
-                                <p>서울특별시 강남구 논현로 523 4층<br/>(역삼동, 노바빌딩)</p>
+                                <p>
+                                    서울특별시 강남구 논현로 523 4층
+                                    <br />
+                                    (역삼동, 노바빌딩)
+                                </p>
                             </div>
                             <div className="w-full mt-4 text-right">
                                 <p className="flex justify-start items-center">
-                                    <span className="w-5 h-5 text-sm bg-green-500 text-white mr-1.5 text-center rounded-full">2</span>
+                                    <span className="w-5 h-5 text-sm bg-green-500 text-white mr-1.5 text-center rounded-full">
+                                        2
+                                    </span>
                                     호선 역삼역 6번 출구 앞
                                 </p>
                                 <p className="flex justify-start items-center mt-1">
-                                    <span className="w-5 h-5 bg-yellow-600 text-white text-sm mr-1.5 text-center rounded-full">9</span>
+                                    <span className="w-5 h-5 bg-yellow-600 text-white text-sm mr-1.5 text-center rounded-full">
+                                        9
+                                    </span>
                                     호선 언주역 7번 출구 앞
                                 </p>
                             </div>
@@ -95,7 +139,7 @@ export const Map: React.FC = () => {
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Map;
